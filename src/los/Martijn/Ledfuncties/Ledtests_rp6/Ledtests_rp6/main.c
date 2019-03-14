f2c1221fc9168d5907160d175d77615f8904c6be
Martijn <mgjhoeks@hotmail.com>
2019-03-14 15:21:21 +0100

    ledtest3.0

--- src/los/Martijn/Ledfuncties/Ledtests_rp6/Ledtests_rp6/main.c
+++ src/los/Martijn/Ledfuncties/Ledtests_rp6/Ledtests_rp6/main.c
@@ -36,16 +36,14 @@ int main(void)
 	
 	while (1) 
     {
-		if (timert(0)%10==0){
+		if (timert(0)%1000==0){
 			toggle_links();	
-		}
-		if (timert(0)%10==0){
 			toggle_rechts();
 		}
-		if (timert(0)%100==0){
+		/*if (timert(0)%100==0){
 			PORTB &= ~(1<<PINB7);
 			PORTC &= ~(1<<PINC4); 
-		}
+		}*/
 	}
 }
 
