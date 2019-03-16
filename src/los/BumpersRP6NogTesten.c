a0a84b93e7085094a7c0b83b023ad4eb7a967bd9
TheBjornUltimatum <48555299+TheBjornUltimatum@users.noreply.github.com>
2019-03-17 00:29:40 +0100

    Add files via upload

---
+++ src/los/BumpersRP6NogTesten.c
@@ -0,0 +1,66 @@
+/*
+ * bumpers.c
+ *
+ * Created: 16-3-2019 23:54:01
+ * Author : Björn van Lieshout
+ code voor gebruik bumpers
+ */ 
+
+#include <avr/io.h>
+#include "functiesBjorn.h"
+
+
+
+int bumperLinks();
+{
+/*
+LED SL6 is portb
+Bumpers links zijn verbonden aan: PORTB, PINB, DDRB, PINB0
+//Situatie: Wanneer de bumper wordt ingedrukt moet hij automatisch achteruit rijden. 
+led sl6 en sl3 zijn statusleds voor de bumpers. Wanneer deze 1 zijn moet de robot achteruit rijden 
+*/	
+	PORTB &= ~(1 << PINB0);
+	DDRB &= ~(1 << PINB0);
+	
+	uint8_t triggerLinks = PINB & PINB0;
+	
+		if(	PORTB &= (1 << PINB0) &&  DDRB &= ~(1 << PINB0)) 
+		{
+		PORTB &= (1 << PINB0);
+		DDRB &= (1 << PINB0);
+		}
+		return triggerLinks;
+
+
+	
+}
+	int bumperRechts();
+{
+/*
+LED SL3 is portc
+zelfde comment als bij bumperLinks
+*/	
+PORTC &= ~(1 << PINC6);
+DDRC &= ~(1 << PINC6);
+
+uint8_t triggerRechts = PINC & PINC6;
+
+if(	PORTC &= (1 << PINC6) &&  DDRC &= ~(1 << PINC6))
+{
+PORTC &= ~(1 << PINC6);
+DDRC &= ~(1 << PINC6);
+}
+return triggerLinks;
+
+
+		
+}
+	
+	
+int returnBumperTrigger();
+{
+
+		return bumperLinks() || bumperRechts();
+    
+}
+
---
+++ src/los/functiesBjorn.h
@@ -0,0 +1,24 @@
+/*
+ * GccApplication14.c
+ *
+ * Created: 16-3-2019 23:49:58
+ * Author : Björn van Lieshout
+ 
+ Bumper rp6 code variabelen voor gebruik main
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+#ifndef functiesBjorn_H_
+#define functiesBjorn_H_
+
+int bumperLinks();
+int bumperRechts();
+int returnBumperTrigger();
+
+#endif /* bumperfunctie_H_ */
+}
+
