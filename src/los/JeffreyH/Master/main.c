0c7eca4d96a510ec98f974a1f8f7a0536ffa4381
Jeff H <j.hoogh91@gmail.com>
2019-03-21 02:01:16 +0100

    Update main.c

--- src/los/JeffreyH/Master/main.c
+++ src/los/JeffreyH/Master/main.c
@@ -30,8 +30,8 @@ void i2c_read(); // read slave response (SLA + 1)
 void lineFeed(); // often re-used code to simulate "enter" in USART
 
 int b = 0; // variable used to store distance to object
-char x, y;
 int sonar = 0;
+char x, y;
 
 int main (void) {
 	initUSART();
@@ -54,22 +54,22 @@ ISR(TIMER1_OVF_vect) {
 	// placeholder timer overflow interrupt
 }
 
-ISR(USART_RX_vect){
-	x = UDR0;
-	switch(x){
-		case 'a':
-		i2c_write(x); i2c_read(); break;
-		case 'w':
-		i2c_write(x); i2c_read(); break;
-		case 'd':
-		i2c_write(x); i2c_read(); break;
-		case 's':
-		i2c_write(x); i2c_read(); break;
-		case 'x':
-		sonar = !sonar; break;
-		default:
-		i2c_write(x); break;
-	}
+ISR(USART0_RX_vect){
+  x = UDR0;
+  switch(x){
+    case 'a':
+    i2c_write(x); i2c_read(); break;
+    case 'w':
+    i2c_write(x); i2c_read(); break;
+    case 'd':
+    i2c_write(x); i2c_read(); break;
+    case 's':
+    i2c_write(x); i2c_read(); break;
+	case 'p':
+	sonar = !sonar; break;
+    default:
+    i2c_write(x); break;
+  }
 }
 
 void sonarSensor(){
@@ -102,7 +102,7 @@ void sonarSensor(){
 		TCNT1 = 0;
 		while ((PINB & (1 << PB5)));
 		b = (TCNT1);
-		if (sonar){
+		if (sonar == 1){
 			printDistance(b / 15);
 		}
 	}
@@ -125,55 +125,56 @@ void initADC() {
 }
 
 void initMaster(void) {
-	PORTC = 0x30; // pull-up resistors for SDA & SCL
-	TWSR = 0;
-	TWBR = (uint8_t)(((F_CPU / F_SCL) - 16 ) / 2);
-	TWCR = (1 << TWEN);
+  PORTD = 0x03;//PORTC = 0x30 voor arduino uno; // pull-up resistors for SDA & SCL (00110000
+  TWSR = 0;
+  TWBR = (uint8_t)(((F_CPU / F_SCL) - 16 ) / 2);
+  TWCR = (1 << TWEN);
 }
 
 void i2c_start(void) {
-	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); // transmit START condition
-	while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
+  TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN); // transmit START condition
+  while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
 }
 
 void i2c_send_byte(uint8_t data) {
-	TWDR = (data);
-	TWCR = (1 << TWINT) | (1 << TWEN); // start transmission
-	while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
+  TWDR = (data);
+  TWCR = (1 << TWINT) | (1 << TWEN); // start transmission
+  while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
 }
 
 uint8_t i2c_read_ack(void) {
-	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA); // start TWI with ACK after reception
-	while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
-	return TWDR; // return received data from TWDR
+  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA); // start TWI with ACK after reception
+  while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
+  return TWDR; // return received data from TWDR
 }
 
 uint8_t i2c_read_nack(void) {
-	TWCR = (1 << TWINT) | (1 << TWEN); // start receiving without acknowledging reception
-	while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
-	return TWDR; // return received data from TWDR
+  TWCR = (1 << TWINT) | (1 << TWEN); // start receiving without acknowledging reception
+  while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
+  return TWDR; // return received data from TWDR
 }
 
 void i2c_stop(void) {
-	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO); // transmit STOP condition
-	while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
+  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO); // transmit STOP condition
+  while ( !(TWCR & (1 << TWINT)) ); // wait for end of transmission
 }
 
 void i2c_write(char x) {
-	i2c_start(); // Start new transmission
-	i2c_send_byte((SLA << 1) + 0); // SLA = slave address, + 0 = master is writing data (write), doesn't expect a response
-	i2c_send_byte(x); // Send data + wait for end of transmission
-	i2c_stop; // Send stop signal (TWSTO)
+  i2c_start(); // Start new transmission
+  i2c_send_byte((SLA << 1) + 0); // SLA = slave address, + 0 = master is writing data (write), doesn't expect a response
+  i2c_send_byte(x); // Send data + wait for end of transmission
+  i2c_stop; // Send stop signal (TWSTO)
 }
 
 void i2c_read() {
-	i2c_start(); // Start new transmission
-	i2c_send_byte((SLA << 1) + 1); // SLA = slave address, + 1 = master is expecting data (read)
-	y = i2c_read_ack(); // Read transmission and send ACK to confirm successful data transfer and store in variable
-	y = i2c_read_nack(); // Read transmission without sending ACK to inform slave to stop sending and store in variable
-	i2c_stop; // Send stop signal (TWSTO)
+  i2c_start(); // Start new transmission
+  i2c_send_byte((SLA << 1) + 1); // SLA = slave address, + 1 = master is expecting data (read)
+  //y = i2c_read_ack(); // Read transmission and send ACK to confirm successful data transfer and store in variable
+  y = i2c_read_nack(); // Read transmission without sending ACK to inform slave to stop sending and store in variable
+  i2c_stop; // Send stop signal (TWSTO)
   
-	UDR0 = y; // print data
+  while (~UCSR0A & (1 << UDRE0));
+  UDR0 = y; // print data
 }
 
 int readADC(int analogInput) {
@@ -214,4 +215,3 @@ void lineFeed() {
 	while (~UCSR0A & (1 << UDRE0));
 	UDR0 = 0x0A;
 }
-
