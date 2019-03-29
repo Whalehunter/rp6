cb98fc5213325c9e3991276d961ac8249bc03b97
Jeffrey Meissen <whale@cock.lu>
2019-03-29 14:15:29 +0100

    Indent fix

--- src/los/Eindfiles/Slave/GccApplication14/GccApplication14/main.c
+++ src/los/Eindfiles/Slave/GccApplication14/GccApplication14/main.c
@@ -23,187 +23,187 @@ void leds();
 char x,y;
 
 int main (void){
-    // Initialisers
-    cli();
-    init();
-	init_leds();
-    //init_usart();
-    sei();
-	i2c_slave_init();
+        // Initialisers
+        cli();
+        init();
+        init_leds();
+        //init_usart();
+        sei();
+        i2c_slave_init();
 
-    while(1)
-        {
-			     //   OCR1A = 50;
-			       // OCR1B = 50;
-				   toggle_rechts();
-				   toggle_links();
-        }
-    return 0;
+        while(1)
+                {
+                        //   OCR1A = 50;
+                        // OCR1B = 50;
+                        toggle_rechts();
+                        toggle_links();
+                }
+        return 0;
 }
 
 ISR(TWI_vect) {
-	
-	switch(TWSR) {
-		// Slave Receiver mode
-		case 0x60: // Own SLA+W has been received; ACK has been returned
-		break;
-		case 0x80: // Addressed with own SLA+W; data received; ACK send
-		x = TWDR; break; 
-		case 0xA0: // A STOP or repeated START condition has been received
-		y = x; break;
-		// Slave Transmitter mode
-		case 0xA8: // Own SLA+R has been received; ACK has been returned
-		rijden(y); break;
-		case 0xB8: // Data byte in TWDR has been send; ACK has been received
-		rijden(y); break;
-		case 0xC0: // Data byte in TWDR has been send; NACK has been received
-		y = 0; break;
-	}
-	TWCR |= (1<<TWINT); // Clear TWINT Flag
+
+        switch(TWSR) {
+                // Slave Receiver mode
+        case 0x60: // Own SLA+W has been received; ACK has been returned
+                break;
+        case 0x80: // Addressed with own SLA+W; data received; ACK send
+                x = TWDR; break;
+        case 0xA0: // A STOP or repeated START condition has been received
+                y = x; break;
+                // Slave Transmitter mode
+        case 0xA8: // Own SLA+R has been received; ACK has been returned
+                rijden(y); break;
+        case 0xB8: // Data byte in TWDR has been send; ACK has been received
+                rijden(y); break;
+        case 0xC0: // Data byte in TWDR has been send; NACK has been received
+                y = 0; break;
+        }
+        TWCR |= (1<<TWINT); // Clear TWINT Flag
 }
 
 // Functions used:
 void i2c_slave_init() {
-	TWSR = 0;
-	TWAR = (SLA<<1); // move address into Address Register
-	TWCR = (1<<TWEA) | (1<<TWIE) | (1<<TWEN);
+        TWSR = 0;
+        TWAR = (SLA<<1); // move address into Address Register
+        TWCR = (1<<TWEA) | (1<<TWIE) | (1<<TWEN);
 }
 
 void init_usart(){
-    UCSRA = 0; // USART status and control registerA uit
-    UCSRB = (1 << RXEN) | (1 << RXCIE); // Enable de USART Reciever
-    UCSRC = (1 << UCSZ1) | (1 << UCSZ0); /* 8 data bits, 1 stop bit */
-    UBRRH = 00;
-    UBRRL = 12; //baudrade 38.4K. voor robotloader
+        UCSRA = 0; // USART status and control registerA uit
+        UCSRB = (1 << RXEN) | (1 << RXCIE); // Enable de USART Reciever
+        UCSRC = (1 << UCSZ1) | (1 << UCSZ0); /* 8 data bits, 1 stop bit */
+        UBRRH = 00;
+        UBRRL = 12; //baudrade 38.4K. voor robotloader
 }
 
 void rijden (char x) {
 
-    switch(x) {
-    case 'w':
-		TWDR = '^';
-        PORTC &= ~(1 << PINC2);
-        PORTC &= ~(1 << PINC3);
-		stuurt(3);
-        break;
-    case 'a':
-        PORTC |= (1 << PINC2);
-        PORTC &= ~(1 << PINC3);
-		stuurt(1);
-		TWDR = '<';
-        break;
-    case 's':
-        PORTC |= (1 << PINC2);
-        PORTC |= (1 << PINC3);
-		TWDR = '.';
-		stuurt(3);
-        break;
-    case 'd':
-        PORTC &= ~(1 << PINC2);
-        PORTC |= (1 << PINC3);
-		stuurt(2);
-		TWDR = '>';
-        break;
-    case '1':
-        OCR1A = 50;
-        OCR1B = 50;
-        break;
-    case '2':
-        OCR1A = 125;
-        OCR1B = 125;
-        break;
-    case '3':
-        OCR1A = 150;
-        OCR1B = 150;
-        break;
-    case 'q':
-        OCR1A = 0;
-        OCR1B = 0;
-	case '8':
-		OCR1A = 0;
-		OCR1B = 0;
-		TWDR = 'X';
-    }
+        switch(x) {
+        case 'w':
+                TWDR = '^';
+                PORTC &= ~(1 << PINC2);
+                PORTC &= ~(1 << PINC3);
+                stuurt(3);
+                break;
+        case 'a':
+                PORTC |= (1 << PINC2);
+                PORTC &= ~(1 << PINC3);
+                stuurt(1);
+                TWDR = '<';
+                break;
+        case 's':
+                PORTC |= (1 << PINC2);
+                PORTC |= (1 << PINC3);
+                TWDR = '.';
+                stuurt(3);
+                break;
+        case 'd':
+                PORTC &= ~(1 << PINC2);
+                PORTC |= (1 << PINC3);
+                stuurt(2);
+                TWDR = '>';
+                break;
+        case '1':
+                OCR1A = 50;
+                OCR1B = 50;
+                break;
+        case '2':
+                OCR1A = 125;
+                OCR1B = 125;
+                break;
+        case '3':
+                OCR1A = 150;
+                OCR1B = 150;
+                break;
+        case 'q':
+                OCR1A = 0;
+                OCR1B = 0;
+        case '8':
+                OCR1A = 0;
+                OCR1B = 0;
+                TWDR = 'X';
+        }
 }
 
 
 void init()
 {
-    TCNT1 = 0;
+        TCNT1 = 0;
 
-    TCCR1A |= (1 << COM1A1)|(1 << COM1B1)|(1 << WGM11); // set output to high on compare + pwm 9-bit
-    TCCR1B |= (1 << WGM13)|(1 << CS10); // no prescaler + pwm, phase & frequency correct
+        TCCR1A |= (1 << COM1A1)|(1 << COM1B1)|(1 << WGM11); // set output to high on compare + pwm 9-bit
+        TCCR1B |= (1 << WGM13)|(1 << CS10); // no prescaler + pwm, phase & frequency correct
 
-    ICR1 = 0x00FF; // set interrupt
-    DDRC |= (1 << PINC2) | (1 << PINC3);
+        ICR1 = 0x00FF; // set interrupt
+        DDRC |= (1 << PINC2) | (1 << PINC3);
 }
 
 /*ISR(USART_RXC_vect) {
-    rijden(UDR);
-}*/
+  rijden(UDR);
+  }*/
 
 void init_leds(){
-	DDRB |= 0b10000000; // Stel pb7 in als output
-	DDRC |= 0b00010000; // Stel pc4 in als output
-	TIMSK = (1<<OCIE0); // Timer overflow interrupt bitje
-	TCCR0 |= (1<<COM00) | (1<<WGM01); // Timer control register COM00 - toggle oc0a on compare match | WGM on ctc mode
-	TCCR0 |= (1<<CS02) | (1<<CS00); // Stelt de prescaler in op 1024. Dan moet de OCA op 77.
-	OCR0 = 77; // OUtput compare ingesteld op 77 (80000/1024)
+        DDRB |= 0b10000000; // Stel pb7 in als output
+        DDRC |= 0b00010000; // Stel pc4 in als output
+        TIMSK = (1<<OCIE0); // Timer overflow interrupt bitje
+        TCCR0 |= (1<<COM00) | (1<<WGM01); // Timer control register COM00 - toggle oc0a on compare match | WGM on ctc mode
+        TCCR0 |= (1<<CS02) | (1<<CS00); // Stelt de prescaler in op 1024. Dan moet de OCA op 77.
+        OCR0 = 77; // OUtput compare ingesteld op 77 (80000/1024)
 }
 
 void toggle_links(){
-	if (stuurt(0) == 1 && timert(0)){
-		PORTB ^= (1<<PINB7);
-		PORTC &= ~(1<<PINC4);
-	} else if(stuurt(0) == 0) {
-		PORTB &= ~(1<<PINB7);
-	}
+        if (stuurt(0) == 1 && timert(0)){
+                PORTB ^= (1<<PINB7);
+                PORTC &= ~(1<<PINC4);
+        } else if(stuurt(0) == 0) {
+                PORTB &= ~(1<<PINB7);
+        }
 }
 
 void toggle_rechts(){
-	if (stuurt(0) == 2 && timert(0)){
-		PORTC ^= (1<<PINC4);
-		PORTB &= ~(1<<PINB7);
-	} else if (stuurt(0) == 0){
-		PORTC &= ~(1<<PINC4);
-	}
+        if (stuurt(0) == 2 && timert(0)){
+                PORTC ^= (1<<PINC4);
+                PORTB &= ~(1<<PINB7);
+        } else if (stuurt(0) == 0){
+                PORTC &= ~(1<<PINC4);
+        }
 }
 
 void leds(){
-	
+
 }
 
 
 uint8_t timert(int x)
 {
-	static uint8_t delay = 0;
+        static uint8_t delay = 0;
 
-	if (x) {
-		delay++;
-		} else if (delay >= DELAYWAARDE) {
-		delay = 0;
-		return 1;
-	}
+        if (x) {
+                delay++;
+        } else if (delay >= DELAYWAARDE) {
+                delay = 0;
+                return 1;
+        }
 
-	return 0;
+        return 0;
 }
 
 int stuurt (int update) {
-	static int richting = 0;
-	
-	if (update) {
-		if (update == 3)
-			richting = 0;
-		else
-			richting = update;
+        static int richting = 0;
 
-		return 0;
-	}
-	
-	return richting;
+        if (update) {
+                if (update == 3)
+                        richting = 0;
+                else
+                        richting = update;
+
+                return 0;
+        }
+
+        return richting;
 }
 
 ISR(TIMER0_COMP_vect) // Interrupt Service Routine
 {
-	timert(1);
+        timert(1);
 }
