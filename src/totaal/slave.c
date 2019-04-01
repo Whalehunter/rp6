1c7253ae82e076777a7aa92073e12adc8d761e46
Jeffrey Meissen <whale@cock.lu>
2019-04-01 10:59:43 +0200

    geen includes meer

--- src/totaal/slave.c
+++ src/totaal/slave.c
@@ -5,7 +5,6 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "rp6.h"
 
 #define SLA 56
 
@@ -16,6 +15,139 @@ void drive(char x);
 uint8_t timert(int x);
 void init_leds();
 void i2c_send(char data);
+
+/* If a setting needs updating in while loop *********************************/
+typedef struct {
+	int speed;
+	int dir;
+} RP6_Update;
+
+/* Speed settings RP6 ********************************************************/
+typedef struct {
+	int cur;
+	int prev;
+	int next;
+} RP6_Speed;
+
+/* Full settings RP6 *********************************************************/
+typedef struct {
+	RP6_Speed speed;
+	char dir;
+	int blinkerCount;
+	RP6_Update update;
+} RP6_Full;
+
+void RP6_SetBlinker(RP6_Full * RP6); /* Update RP6 knipperlicht */
+void RP6_SetSpeed(RP6_Full * RP6, char speed); /* Update RP6 speed geleidelijk */
+void RP6_SetDirection(RP6_Full * RP6, char direction); /* Update RP6 richting */
+char RP6_GetDirection(RP6_Full * RP6);		       /* Get current direction of RP6 */
+void RP6_SetCurrentSpeed(RP6_Full * RP6);	 /* Set acceleration for next item */
+
+/*****************************************************************************/
+/*      De "Execute" functies zijn in princepe alleen voor RP6_Execute.      */
+/*****************************************************************************/
+void RP6_Execute(RP6_Full * RP6);		 /* Execute RP6 settings */
+void RP6_Execute_Direction(RP6_Full * RP6);	 /* Execute drive direction */
+void RP6_Execute_Blinker(RP6_Full * RP6);	 /* Execute blinker toggles */
+void RP6_Execute_Speed(RP6_Full * RP6); /* Execute RP6 speed */
+
+void RP6_SetBlinker(RP6_Full * RP6) {
+        RP6->blinkerCount = 20;
+}
+
+void RP6_SetSpeed(RP6_Full * RP6, char speed) {
+        int newSpeed = 50 * (int)speed;
+        if (RP6->speed.next != newSpeed) {
+                RP6->speed.next = newSpeed;
+                RP6->update.speed = 1;
+        }
+}
+
+void RP6_SetDirection(RP6_Full * RP6, char direction) {
+        if (direction != RP6->dir) {
+                RP6->dir = direction;
+                RP6->update.dir = 1;
+                RP6_SetBlinker(RP6);
+        }
+}
+
+char RP6_GetDirection(RP6_Full * RP6) {
+        switch(RP6->dir) {
+        case 'w': return '^';
+        case 'a': return '<';
+        case 's': return '.';
+        case 'd': return '>';
+        default: return '?';
+        }
+}
+
+void RP6_SetCurrentSpeed(RP6_Full * RP6) {
+        int inc = 1;
+        if (RP6->speed.cur > RP6->speed.next) {
+                inc = -1;
+        }
+
+        RP6->speed.cur += inc;
+
+        if (RP6->speed.cur == RP6->speed.next) {
+                RP6->update.speed = 0;
+        }
+}
+
+void RP6_Execute(RP6_Full * RP6) {
+        if (RP6->update.dir) {
+                RP6_Execute_Direction(RP6);
+        }
+        if (RP6->blinkerCount > 20) {
+                RP6_Execute_Blinker(RP6);
+        }
+        if (RP6->update.speed) {
+                RP6_Execute_Speed(RP6);
+        }
+}
+
+void RP6_Execute_Direction(RP6_Full * RP6) {
+        switch(RP6->dir) {
+        case 'w':
+                PORTC &= ~(1 << PINC2);
+                PORTC &= ~(1 << PINC3);
+                break;
+        case 'a':
+                PORTC |= (1 << PINC2);
+                PORTC &= ~(1 << PINC3);
+                break;
+        case 's':
+                PORTC |= (1 << PINC2);
+                PORTC |= (1 << PINC3);
+                break;
+        case 'd':
+                PORTC &= ~(1 << PINC2);
+                PORTC |= (1 << PINC3);
+                break;
+        }
+}
+
+void RP6_Execute_Blinker(RP6_Full * RP6) {
+        switch(RP6->dir) {
+        case 'a':
+                PORTC ^= (1 << PINC4); /* Toggle left blinker */
+                PORTB &= ~(1 << PINB7); /* Disable right blinker */
+                break;
+        case 'd':
+                PORTC &= ~(1 << PINC4); /* Disable left blinker */
+                PORTB ^= (1 << PINB7);  /* Toggle right blinker */
+                break;
+        default:
+                PORTC &= ~(1 << PINC4); /* Disable left blinker */
+                PORTB &= ~(1 << PINB7); /* Disable right blinker */
+                break;
+        }
+}
+
+void RP6_Execute_Speed(RP6_Full * RP6) {
+        OCR1A = RP6->speed.cur;
+        OCR1B = RP6->speed.cur;
+}
 
 static RP6_Full rp6 = {{0,0,0},	/* Speed */
 		       'w',	/* Direction */
